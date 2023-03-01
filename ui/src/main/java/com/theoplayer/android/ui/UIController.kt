7e270e1f3dc4c666f26f33bdec0cfd40386fe32c
Mattias Buelens <mattias.buelens@theoplayer.com>
2023-03-01 16:16:14 +0100

    Push lifecycle events to THEOplayerView

--- ui/src/main/java/com/theoplayer/android/ui/UIController.kt
+++ ui/src/main/java/com/theoplayer/android/ui/UIController.kt
@@ -4,12 +4,16 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalInspectionMode
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
 import com.theoplayer.android.api.THEOplayerConfig
 import com.theoplayer.android.api.THEOplayerView
 import com.theoplayer.android.api.source.SourceDescription
@@ -21,8 +25,8 @@ fun UIController(
     topChrome: (@Composable ColumnScope.() -> Unit)? = null,
     bottomChrome: (@Composable ColumnScope.() -> Unit)? = null
 ) {
-    val theoplayerView = remember { mutableStateOf<THEOplayerView?>(null) }
-    val state = rememberPlayerState(theoplayerView.value?.player)
+    val theoplayerView = rememberTHEOplayerView(config)
+    val state = rememberPlayerState(theoplayerView.player)
 
     if (LocalInspectionMode.current) {
         Box(
@@ -33,16 +37,7 @@ fun UIController(
     } else {
         AndroidView(
             modifier = Modifier.fillMaxSize(),
-            factory = { context ->
-                THEOplayerView(context, config).also {
-                    it.settings.isFullScreenOrientationCoupled = true
-                    it.player.source = SourceDescription.Builder(
-                        TypedSource.Builder("https://amssamples.streaming.mediaservices.windows.net/7ceb010f-d9a3-467a-915e-5728acced7e3/ElephantsDreamMultiAudio.ism/manifest(format=mpd-time-csf)")
-                            .build()
-                    ).build()
-                    theoplayerView.value = it
-                }
-            })
+            factory = { theoplayerView })
     }
 
     CompositionLocalProvider(LocalTHEOplayer provides state) {
@@ -52,4 +47,36 @@ fun UIController(
             bottomChrome?.let { it() }
         }
     }
+}
+
+@Composable
+fun rememberTHEOplayerView(config: THEOplayerConfig): THEOplayerView {
+    val context = LocalContext.current
+    val theoplayerView = remember {
+        THEOplayerView(context, config).apply {
+            settings.isFullScreenOrientationCoupled = true
+            player.source = SourceDescription.Builder(
+                TypedSource.Builder("https://amssamples.streaming.mediaservices.windows.net/7ceb010f-d9a3-467a-915e-5728acced7e3/ElephantsDreamMultiAudio.ism/manifest(format=mpd-time-csf)")
+                    .build()
+            ).build()
+        }
+    }
+
+    val lifecycle = LocalLifecycleOwner.current.lifecycle
+    DisposableEffect(key1 = lifecycle, key2 = theoplayerView) {
+        val lifecycleObserver = LifecycleEventObserver { _, event ->
+            when (event) {
+                Lifecycle.Event.ON_RESUME -> theoplayerView.onResume()
+                Lifecycle.Event.ON_PAUSE -> theoplayerView.onPause()
+                Lifecycle.Event.ON_DESTROY -> theoplayerView.onDestroy()
+                else -> {}
+            }
+        }
+        lifecycle.addObserver(lifecycleObserver)
+        onDispose {
+            lifecycle.removeObserver(lifecycleObserver)
+        }
+    }
+
+    return theoplayerView
 }
