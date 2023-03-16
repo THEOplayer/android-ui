731fa25d123fe54f53a7b8dcb023ad9ab1a6169a
Mattias Buelens <mattias.buelens@theoplayer.com>
2023-03-16 17:32:18 +0100

    Add showRemaining to CurrentTimeDisplay

--- ui/src/main/java/com/theoplayer/android/ui/CurrentTimeDisplay.kt
+++ ui/src/main/java/com/theoplayer/android/ui/CurrentTimeDisplay.kt
@@ -9,14 +9,21 @@ import androidx.compose.ui.graphics.Color
 fun CurrentTimeDisplay(
     modifier: Modifier = Modifier,
     color: Color = Color.Unspecified,
+    showRemaining: Boolean = false,
     showDuration: Boolean = false,
 ) {
     val state = LocalTHEOplayer.current
     val currentTime = state?.currentTime ?: 0.0
     val duration = state?.duration ?: Double.NaN
 
+    val time = if (showRemaining) {
+        -(duration - currentTime)
+    } else {
+        currentTime
+    }
+
     val text = StringBuilder()
-    text.append(formatTime(currentTime, duration))
+    text.append(formatTime(time, duration))
     if (showDuration) {
         text.append(" / ")
         text.append(formatTime(duration))
