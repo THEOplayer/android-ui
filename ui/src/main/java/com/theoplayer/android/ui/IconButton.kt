5afe2d91d7eca799d3327eecfdaf076fd0512a74
Mattias Buelens <mattias.buelens@theoplayer.com>
2023-03-28 14:38:34 +0200

    Document IconButton

--- ui/src/main/java/com/theoplayer/android/ui/IconButton.kt
+++ ui/src/main/java/com/theoplayer/android/ui/IconButton.kt
@@ -1,8 +1,10 @@
 package com.theoplayer.android.ui
 
+import androidx.compose.foundation.interaction.Interaction
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
@@ -11,9 +13,23 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 /**
- * Based on [androidx.compose.material3.IconButton]
- *  - Replaced `Modifier.size()` with `Modifier.defaultMinSize()`,
- *    to allow for larger icons.
+ * <a href="https://m3.material.io/components/icon-button/overview" class="external" target="_blank">Material Design standard icon button</a>.
+ *
+ * Equivalent to [androidx.compose.material3.IconButton],
+ * but uses [Modifier.defaultMinSize] instead of [Modifier.size] to allow for larger icons.
+ *
+ * @param onClick called when this button is clicked
+ * @param modifier the [Modifier] to be applied to this button
+ * @param enabled controls the enabled state of this button. When `false`, this component will not
+ * respond to user input, and it will appear visually disabled and disabled to accessibility
+ * services.
+ * @param colors [ButtonColors] that will be used to resolve the colors for this button in different
+ * states. See [defaultIconButtonColors].
+ * @param contentPadding the spacing values to apply internally between the container and the
+ * content
+ * @param interactionSource the [MutableInteractionSource] representing the stream of [Interaction]s
+ * for this button. You can create and pass in your own `remember`ed instance to observe
+ * [Interaction]s and customize the appearance / behavior of this button in different states.
  */
 @Composable
 fun IconButton(
@@ -45,7 +61,15 @@ private const val DisabledIconOpacity = 0.38f
 private val IconButtonSize = 40.dp
 
 /**
- * Equivalent to [IconButtonDefaults.iconButtonColors]
+ * Creates a [ButtonColors] that represents the default colors used in a [IconButton].
+ *
+ * Equivalent to [IconButtonDefaults.iconButtonColors], but as [ButtonColors]
+ * instead of [androidx.compose.material3.IconButtonColors].
+ *
+ * @param containerColor the container color of this icon button when enabled.
+ * @param contentColor the content color of this icon button when enabled.
+ * @param disabledContainerColor the container color of this icon button when not enabled.
+ * @param disabledContentColor the content color of this icon button when not enabled.
  */
 @Composable
 fun defaultIconButtonColors(
