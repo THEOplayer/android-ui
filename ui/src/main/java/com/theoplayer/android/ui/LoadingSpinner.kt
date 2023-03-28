5f6eba5c834b5b4eb0f7a9c135c18ec5575ace47
Mattias Buelens <mattias.buelens@theoplayer.com>
2023-03-28 17:25:27 +0200

    Make loading spinner's delay configurable

--- ui/src/main/java/com/theoplayer/android/ui/LoadingSpinner.kt
+++ ui/src/main/java/com/theoplayer/android/ui/LoadingSpinner.kt
@@ -10,6 +10,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Dp
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.DurationUnit
 
 /**
  * An indicator that shows whether the player is currently waiting for more data to resume playback.
@@ -17,19 +20,22 @@ import androidx.compose.ui.unit.Dp
  * @param modifier the [Modifier] to be applied to this indicator
  * @param color color of this progress indicator
  * @param strokeWidth stroke width of this progress indicator
+ * @param delay delay for the progress indicator to become visible after the player starts buffering.
+ * In general, it's less jarring to *not* show the indicator for very short buffering periods.
  */
 @Composable
 fun LoadingSpinner(
     modifier: Modifier = Modifier,
     color: Color = ProgressIndicatorDefaults.circularColor,
-    strokeWidth: Dp = ProgressIndicatorDefaults.CircularStrokeWidth
+    strokeWidth: Dp = ProgressIndicatorDefaults.CircularStrokeWidth,
+    delay: Duration = 500.milliseconds
 ) {
     val state = LocalTHEOplayer.current
     val loading = state?.loading ?: false
     AnimatedVisibility(
         visible = loading,
         // Show the loading spinner only after a small delay
-        enter = fadeIn(snap(delayMillis = 500)),
+        enter = fadeIn(snap(delayMillis = delay.toInt(DurationUnit.MILLISECONDS))),
         // Hide the loading spinner immediately when loaded
         exit = ExitTransition.None
     ) {
