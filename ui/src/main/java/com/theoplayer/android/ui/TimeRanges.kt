1226f074a49b4d2ed657cef2213e5a1cd0600194
Mattias Buelens <mattias.buelens@theoplayer.com>
2023-03-29 15:02:49 +0200

    Refactor TimeRanges

--- ui/src/main/java/com/theoplayer/android/ui/TimeRanges.kt
+++ ui/src/main/java/com/theoplayer/android/ui/TimeRanges.kt
@@ -1,15 +1,15 @@
 package com.theoplayer.android.ui
 
-data class TimeRanges(val ranges: List<Pair<Double, Double>>) {
+data class TimeRanges(val ranges: List<ClosedFloatingPointRange<Double>>) {
     val firstStart: Double?
-        get() = ranges.firstOrNull()?.first
+        get() = ranges.firstOrNull()?.start
     val lastEnd: Double?
-        get() = ranges.lastOrNull()?.second
+        get() = ranges.lastOrNull()?.endInclusive
 
     companion object {
         @JvmStatic
         fun fromTHEOplayer(ranges: com.theoplayer.android.api.timerange.TimeRanges): TimeRanges {
-            return TimeRanges(ranges.map { range -> Pair(range.start, range.end) })
+            return TimeRanges(ranges.map { range -> range.start.rangeTo(range.end) })
         }
     }
 }
