6161cbf69b0f7e24f6524c26fe9f80f2ff0edb61
Mattias Buelens <mattias.buelens@theoplayer.com>
2023-04-05 15:03:34 +0200

    Add build type to demo app to test Maven dependency

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,6 +11,7 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
         maven { url = uri("https://jitpack.io") }
+        maven { url = uri("https://maven.pkg.github.com/THEOplayer/android-ui") }
     }
 }
 rootProject.name = "THEOplayer Android UI"
