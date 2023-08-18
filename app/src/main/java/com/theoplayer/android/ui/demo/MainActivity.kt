a50dbb42347950c075039752c573c9ff444b10e2
Tom Van Laerhoven <tom.vanlaerhoven@gmail.com>
2023-08-18 12:41:39 +0200

    Add demo source with ad

--- app/src/main/java/com/theoplayer/android/ui/demo/MainActivity.kt
+++ app/src/main/java/com/theoplayer/android/ui/demo/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.theoplayer.android.ui.demo
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,13 +15,20 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.theoplayer.android.api.THEOplayerConfig
+import com.theoplayer.android.api.ads.ima.GoogleImaAdErrorEvent
+import com.theoplayer.android.api.ads.ima.GoogleImaAdEventType
+import com.theoplayer.android.api.ads.ima.GoogleImaIntegrationFactory.createGoogleImaIntegration
+import com.theoplayer.android.api.event.player.PlayerEventTypes
 import com.theoplayer.android.api.source.SourceDescription
 import com.theoplayer.android.api.source.TypedSource
+import com.theoplayer.android.api.source.addescription.GoogleImaAdDescription
 import com.theoplayer.android.ui.DefaultUI
 import com.theoplayer.android.ui.demo.nitflex.NitflexUI
 import com.theoplayer.android.ui.demo.nitflex.theme.NitflexTheme
 import com.theoplayer.android.ui.rememberPlayer
 import com.theoplayer.android.ui.theme.THEOplayerTheme
+
+private const val TAG = "DEMO"
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,14 +45,35 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainContent() {
-    val source = SourceDescription.Builder(
-        TypedSource.Builder("https://cdn.theoplayer.com/video/elephants-dream/playlist.m3u8")
-            .build()
-    ).build()
-
     val player = rememberPlayer()
-    LaunchedEffect(player, source) {
+
+    val adTag = "https://cdn.theoplayer.com/demos/ads/vast/dfp-preroll-no-skip.xml"
+    val imaAdDescription =
+        GoogleImaAdDescription.Builder(adTag)
+            .timeOffset("start")
+            .build()
+    val source = SourceDescription.Builder(
+        TypedSource.Builder("https://cdn.theoplayer.com/video/dash/webvtt-embedded-in-isobmff/Manifest.mpd")
+            .build()
+    )
+        .ads(imaAdDescription)
+        .build()
+
+    DisposableEffect(player) {
+        player.theoplayerView?.let { playerView ->
+            val imaIntegration = createGoogleImaIntegration(playerView)
+            player.player?.addIntegration(imaIntegration)
+        }
+        player.player?.addEventListener(PlayerEventTypes.PLAY) { Log.d(TAG, "PLAY") }
+        player.player?.addEventListener(PlayerEventTypes.PLAYING) { Log.d(TAG, "PLAYING") }
+        player.player?.addEventListener(PlayerEventTypes.PAUSE) { Log.d(TAG, "PAUSE") }
+        player.player?.ads?.addEventListener(GoogleImaAdEventType.AD_ERROR) { event ->
+            Log.d(TAG, "IMA ERROR: ${(event as GoogleImaAdErrorEvent).adError?.errorType}")
+        }
         player.source = source
+        onDispose {
+            // Clean-up: remove listeners
+        }
     }
 
     var themeMenuOpen by remember { mutableStateOf(false) }
