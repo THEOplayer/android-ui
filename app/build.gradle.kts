1fccdd11798bd2ecaad950db460be0a3b0bf0887
Mattias Buelens <mattias.buelens@theoplayer.com>
2023-11-20 15:56:01 +0100

    Revert to targetSdk 33 for now

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     defaultConfig {
         applicationId = "com.theoplayer.android.ui.demo"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
