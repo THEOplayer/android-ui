f9eb7ee2b1697ba3247a47b919a3c009bdbe2624
Mattias Buelens <mattias.buelens@theoplayer.com>
2024-12-11 11:02:24 +0100

    Set compileSdk to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.theoplayer.android.ui.demo"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.theoplayer.android.ui.demo"
