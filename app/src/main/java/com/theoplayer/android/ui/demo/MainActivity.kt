d4fb670bfdab667787af4560e6e17a849efd8646
Mattias Buelens <mattias.buelens@theoplayer.com>
2025-09-04 13:21:51 +0200

    Use Enum.entries

--- app/src/main/java/com/theoplayer/android/ui/demo/MainActivity.kt
+++ app/src/main/java/com/theoplayer/android/ui/demo/MainActivity.kt
@@ -15,9 +15,23 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Brush
 import androidx.compose.material.icons.rounded.Movie
 import androidx.compose.material.icons.rounded.Refresh
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
@@ -217,7 +231,7 @@ fun SelectThemeDialog(
                     style = MaterialTheme.typography.headlineSmall
                 )
                 LazyColumn {
-                    items(items = PlayerTheme.values()) {
+                    items(items = PlayerTheme.entries) {
                         ListItem(
                             headlineContent = { Text(text = it.title) },
                             leadingContent = {
