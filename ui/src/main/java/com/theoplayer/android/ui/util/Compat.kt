834a6089b7e1d36c4d063acbb3965c809cfb73f8
Mattias Buelens <mattias.buelens@theoplayer.com>
2026-03-17 17:26:25 +0100

    Use helper class instead of reflection

--- ui/src/main/java/com/theoplayer/android/ui/util/Compat.kt
+++ ui/src/main/java/com/theoplayer/android/ui/util/Compat.kt
@@ -1,24 +1,30 @@
 package com.theoplayer.android.ui.util
 
+import androidx.annotation.DoNotInline
 import com.theoplayer.android.api.player.track.texttrack.TextTrack
-import java.lang.reflect.Method
 
 /**
  * Returns [TextTrack.getCaptionChannel], if available.
  */
 internal val TextTrack.captionChannelCompat: Int?
-    get() = textTrackCaptionChannelGetter?.invoke(this) as? Int
+    get() {
+        // TextTrack.getCaptionChannel was added in THEOplayer 10.13.0.
+        return if (theoplayerVersion >= version1013) {
+            TheoPlayer1013Impl.getTextTrackCaptionChannel(this)
+        } else null
+    }
 
-private val textTrackCaptionChannelGetter: Method? by lazy {
-    try {
-        TextTrack::class.java.getMethod("getCaptionChannel").also {
-            check(it.returnType.kotlin == Int::class)
-        }
-    } catch (_: NoSuchMethodException) {
-        null
-    } catch (_: SecurityException) {
-        null
-    } catch (_: IllegalStateException) {
-        null
+private val version1013 = Version(major = 10, minor = 13, patchAndPrerelease = "0")
+
+/**
+ * This class must be loaded **only** with THEOplayer 10.13.0 or higher.
+ *
+ * This uses the same pattern as AndroidX AppCompat,
+ * see e.g. [androidx.appcompat.app.AppCompatDelegate.Api33Impl]
+ */
+private class TheoPlayer1013Impl private constructor() {
+    companion object {
+        @DoNotInline
+        fun getTextTrackCaptionChannel(track: TextTrack): Int? = track.captionChannel
     }
 }
